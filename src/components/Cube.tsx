9ad92ea10b54fc0f9d1c2177ca4954a92aa1e023
albnjohanssn <albin.johansson0302@gmail.com>
2025-04-09 09:09:04 +0300

    Next.JS - 3D Application Test

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,19 +1,34 @@
+import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react';
 
-export default function Cube() {
+export default function InteractiveCube() {
   const cubeRef = useRef(null);
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
 
+  // Animate the cube by rotating it on every frame
   useFrame(() => {
     if (cubeRef.current) {
       cubeRef.current.rotation.x += 0.02;
       cubeRef.current.rotation.y += 0.02;
     }
   });
+
   return (
-    <mesh ref={cubeRef} rotation={[0, 1, 1]}>
+    <mesh
+      ref={cubeRef}
+      rotation={[0, 1, 1]}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
       <boxGeometry args={[3, 3, 3]} />
-      <meshStandardMaterial color='silver' metalness={0.3} roughness={0.5} />
+      <meshStandardMaterial
+        color={hovered ? 'green' : 'silver'}
+        metalness={0.3}
+        roughness={0.5}
+      />
     </mesh>
   );
 }
